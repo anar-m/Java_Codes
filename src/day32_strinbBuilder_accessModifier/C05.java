552010caa6a6482b62d387474402982f77f47d22
anar-m <mustafa5225anar@gmail.com>
2023-08-07 22:49:07 +0300

    32.gunSonu_Codes

---
+++ src/day32_strinbBuilder_accessModifier/C05.java
@@ -0,0 +1,14 @@
+package day32_strinbBuilder_accessModifier;
+
+public class C05 {
+
+    private int privateSayi;
+    int defaultSayi;
+    protected int protectedSayi;
+    public int publicSayi;
+
+
+
+
+
+}
