bee97fc2d9e3fbb4231aa059e8f8c0a1ab85d5a2
anar-m <mustafa5225anar@gmail.com>
2023-08-15 22:56:29 +0300

    JAva_Codes

---
+++ src/day37_overridin_pollymorphisim/D_Child.java
@@ -0,0 +1,24 @@
+package day37_overridin_pollymorphisim;
+
+public class D_Child extends C_MecburiOlmayanParent{
+    /*
+    Normal olarka kullanılan inheritance da parent classs lar pasiftir.
+    bütün ipler child'ın elindedir.
+    istediği claas ı parent edinebiir,
+    isrediği methodlerı parent tan kullanır
+    istemediklerini override eder;
+    istediği yeni method'ları kendisine ekler.
+
+    Abstraction bu gidişata dur der
+
+    yine parent seçme hakkı child'dadır
+    AMMA parent edinmek istersen
+    elin mahkum söylediğim methodları override ederek kendine uyarlayacaksın
+    yani child claas'ın mutlaka sahip olması ve kendine uyarlaması gereken methodlrı
+    PArent class belirliyor
+
+     */
+
+
+
+}
